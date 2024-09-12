b0bd13baf9cabe83ec663af3400ec3307a16dc0d
SeoKyungyee <skpink28@gmail.com>
2024-09-12 16:39:24 +0900

    2주차 실습파일

---
+++ 2week/ex0.py
@@ -0,0 +1,13 @@
+print("1개의 정수를 입력하세요.")
+
+num = int(input("정수 입력: "))
+prime = (num % 2) == 0
+print("짝수이다 - ", prime)
+
+# num = int(input())
+# num1 = num % 2
+
+# if num1 == 0:
+#    print("짝수이다 - True")
+# if num1 != 0:
+#    print("짝수이다 - False")
