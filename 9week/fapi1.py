0e72240ce8d682d0bd1bc4d4af61e1ed4bd986f2
SeoKyungyee <skpink28@gmail.com>
2024-11-14 16:25:39 +0900

    10주차 실습파일

--- 9week/fapi1.py
+++ 9week/fapi1.py
@@ -23,6 +23,7 @@ async def plus_form(num1: int = Form(...), num2 : int = Form(...), num3 : int = 
 from fastapi import File, UploadFile
 import shutil
 from pathlib import Path
+from fastapi.responses import FileResponse
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
@@ -33,6 +34,14 @@ async def create_upload_file(file: UploadFile = File(...)):
         shutil.copyfileobj(file.file, buffer)
         
     return {"filename" : file.filename, "location" : str(save_path)}
+
+@app.get("/files/{filename}")
+async def get_file(filename: str):
+    file_path = Path("static/uploads") / filename
+    if file_path.is_file():
+        return FileResponse(path=file_path, filename=filename)
+    else:
+        raise HTTPException(status_code=404, detail="File not found")
 
 from fastapi.staticfiles import StaticFiles
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
